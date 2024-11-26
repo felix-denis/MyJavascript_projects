cdf17b6e92bb8ba90e8f32425f7f574313b53728
denis <denismugambi350@gmail.com>
2024-11-26 10:01:38 +0300

    I have changed some lines to ensure there is no code redundancy in the js file

--- Rock_Paper_Scissors/index.js
+++ Rock_Paper_Scissors/index.js
@@ -9,24 +9,21 @@ let scorePlayer = 0;
 let scoreComputer = 0;
 
 button.addEventListener("click", () => {
-   playerDisplay.textContent = `Player:`;
-   computerDisplay.textContent = `Computer:`;
-    result.textContent = "";
-    scorePlayer = 0;
-    scoreComputer = 0;
-    playerScore.textContent = "Player score: 0";
-    computerScore.textContent = "Computer score: 0";
-})
+  playerDisplay.textContent = `Player:`;
+  computerDisplay.textContent = `Computer:`;
+  result.textContent = "";
+  scorePlayer = 0;
+  scoreComputer = 0;
+  playerScore.textContent = "Player score: 0";
+  computerScore.textContent = "Computer score: 0";
+});
 
 function playGame(playerChoice) {
   const computerChoice = choices[Math.floor(Math.random() * 3)];
   let result1;
-
+   
   if (playerChoice === computerChoice) {
-    result.textContent = "IT'S A TIE!";
-    result.classList.remove("green", "red");
-    playerDisplay.textContent = `Player: ${playerChoice}`;
-    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    result1 = "IT'S A TIE!";
   } else {
     switch (playerChoice) {
       case "rock":
@@ -39,31 +36,24 @@ function playGame(playerChoice) {
         result1 = computerChoice === "paper" ? "YOU WIN!" : "YOU LOSE!";
         break;
     }
-    playerDisplay.textContent = `Player: ${playerChoice}`;
-    computerDisplay.textContent = `Computer: ${computerChoice}`;
-    result.textContent = result1;
+  }
+  playerDisplay.textContent = `Player: ${playerChoice}`;
+  computerDisplay.textContent = `Computer: ${computerChoice}`;
+  result.textContent = result1;
 
-     
-   
+  result.classList.remove("green", "red");
 
-    switch (result1) {
-      case "YOU WIN!":
-        result.classList.add("green");
-        result.classList.remove("red");
-        break;
-      case "YOU LOSE!":
-        result.classList.add("red");
-        result.classList.remove("green");
-        break;
-    }
-
-    if (result1 === "YOU WIN!") {
+  switch (result1) {
+    case "YOU WIN!":
+      result.classList.add("green");
       scorePlayer++;
       playerScore.textContent = "Player score:" + scorePlayer;
-    } else {
+      break;
+    case "YOU LOSE!":
+      result.classList.add("red");
       scoreComputer++;
       computerScore.textContent = "Computer score:" + scoreComputer;
-    }
-   
+      break;
   }
+ 
 }
